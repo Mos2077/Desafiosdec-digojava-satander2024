a1887bdb16b96e425a58ea61473009c8095fefc9
Matheus O Silva <78383751+Mos2077@users.noreply.github.com>
2024-04-27 17:51:37 -0300

    Create desafio10.java

---
+++ desafio10.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+
+        double limiteDiario = scanner.nextDouble();
+        double limiteRestante = limiteDiario;
+
+        for (;;) {
+            System.out.println();
+            double valorSaque = scanner.nextDouble();
+
+            if (valorSaque == 0) {
+                System.out.println("Transacoes encerradas.");
+                break;
+            }
+
+            if (valorSaque > limiteRestante) {
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break;
+            }
+
+            limiteRestante -= valorSaque;
+            System.out.println("Saque realizado. Limite restante: " + limiteRestante);
+        }
+
+        // Fechamos o Scanner para evitar vazamento de recursos:
+        scanner.close(); 
+    }
+}
